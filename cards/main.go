45b5fc9c46c0bc39682bc22861018d108d4652b4
luucasor <lucas@gatilabs.com.br>
2019-02-05 23:42:07 -0200

    Exemplo do uso indevído de this... em go não devemos utilizar 'this' ou 'self' a convenção é utilizar uma abreviação para a atribuição do tipo dentro do método receiver. Por exemplo: func (d deck) print()

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 	cards.print()
+
+	var ls laptopSize = 35.4
+	fmt.Println(ls.getSizeOfLaptop())
 }
 
 func newCard() string {
