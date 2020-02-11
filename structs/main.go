7d6bc24eeedb08e590c8fd3611055b8b7b4081f4
luucasor <lucas@jornadatecnologia.com>
2020-02-11 18:39:24 -0300

    Exemplo de struct com sub struct. person.contactInfo

--- structs/main.go
+++ structs/main.go
@@ -2,17 +2,28 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
-func main(){
-	var alex person
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+func main(){
+	jim := person {
+		firstName: "Jim",
+		lastName: "Party",
+		contact: contactInfo {
+			email: "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
 	
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	fmt.Println(jim)
+	fmt.Printf("%+v", jim)
 }
