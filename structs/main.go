b0dc5759e01e32d43a28ee3720b1b9b9555bdbba
luucasor <lucas@jornadatecnologia.com>
2020-02-11 19:02:35 -0300

    Criando função para receiver person. Em destaque 'problema' com atribuições e ponteiros, resolução será apresentada na próxima aula do curso.

--- structs/main.go
+++ structs/main.go
@@ -3,27 +3,34 @@ package main
 import "fmt"
 
 type contactInfo struct {
-	email string
+	email   string
 	zipCode int
 }
 
 type person struct {
 	firstName string
-	lastName string
-	contact contactInfo
+	lastName  string
+	contactInfo
 }
 
-
-func main(){
-	jim := person {
+func main() {
+	jim := person{
 		firstName: "Jim",
-		lastName: "Party",
-		contact: contactInfo {
-			email: "jim@gmail.com",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
-	
-	fmt.Println(jim)
-	fmt.Printf("%+v", jim)
+
+	jim.updateFirstName("Jimmy")
+	jim.print()
+}
+
+func (p person) updateFirstName(newFirstName string){
+	p.firstName = newFirstName
+} 
+
+func (p person) print(){
+	fmt.Printf("%+v", p)
 }
